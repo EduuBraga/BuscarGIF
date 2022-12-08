0d74709a5039254ca18a25bd49fa15b821285e79
Eduardo Braga <chocolate.blindado123@gmail.com>
2022-12-08 10:53:37 -0300

    Feat: add e removendo feedbacks

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const form = document.querySelector('#form')
+const feedback = document.querySelector('.feedback')
+const inputSearch = document.querySelector('#search')
 
-const getGif = async (valueInputSearch) => {
+const getGif = async valueInputSearch => {
   const key = '7WV8kKMCQP47wLG9EkxFly58Z5FPnpif'
-  const valueInput = valueInputSearch
+  const valueInput = valueInputSearch.replace(/\s+/g, '%20')
   const url =
   `https://api.giphy.com/v1/gifs/search?api_key=${key}&limit=1&q=${valueInput}`
 
   try {
-    const { data: dataGif, meta } = await (await fetch(url)).json()
+    const { data, meta } = await (await fetch(url)).json()
 
     if (meta.msg !== 'OK') {
       throw new Error('Error')
     }
 
-    return dataGif[0]
+    return data[0].images.downsized_medium.url
   } catch (error) {
     console.log('Gif não encontrado')
   }
@@ -21,25 +23,41 @@ const getGif = async (valueInputSearch) => {
 
 const implementGif = (url, alt) => {
   const gif = document.createElement('IMG')
-  const out = document.querySelector('.out')
+  const gifsContainer = document.querySelector('.gifs')
 
   gif.src = url
   gif.alt = alt
   
-  out.prepend(gif)
+  gifsContainer.prepend(gif)
+}
+
+const hiddenOrShowFeedback = msg => {
+  if(msg === 'show'){
+    feedback.style = 'display: block'
+    inputSearch.classList.add('border-feedback')
+    return
+  }
+
+  feedback.style = 'display: none'
+  inputSearch.classList.remove('border-feedback')
 }
 
 form.addEventListener('submit', async event => {
   event.preventDefault()
 
   const valueSearch = form.search.value
-  const dataGif = await getGif(valueSearch)
-  const gifUrl = dataGif.images.downsized_medium.url
+  const gifUrl = await getGif(valueSearch)
 
-  if(!dataGif){
-    console.log('error')
+  //removendo feedback
+  hiddenOrShowFeedback('hidden')
+
+  //mostrando o feedback de erro, caso o gif não seja encontrado
+  if(!gifUrl){
+    hiddenOrShowFeedback('show')
     return
   }
 
+  //Implemtando GIF na DOM
+  inputSearch.value = ''
   implementGif(gifUrl, valueSearch)
 })
